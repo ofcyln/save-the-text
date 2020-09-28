9d6845509a47c1279bf820b794763a2d90d567b3
Osman Fikret Ceylan <ofcyln@gmail.com>
2020-09-28 15:09:28 +0200

    Cleanup default template content of the Angular seed project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'save-the-text-app';
-}
+export class AppComponent {}
