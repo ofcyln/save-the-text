3b944be5cbe5736da786c73bb294ca03dc4aaeaa
Osman Fikret Ceylan <ofcyln@gmail.com>
2024-02-09 19:18:28 +0100

    chore: angular flex layout uninstalled

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [CommonModule, AppRoutingModule, MaterialModule, FlexLayoutModule],
-  exports: [NotFoundComponent, AppRoutingModule, MaterialModule, FlexLayoutModule],
+  imports: [CommonModule, AppRoutingModule, MaterialModule],
+  exports: [NotFoundComponent, AppRoutingModule, MaterialModule],
 })
 export class CoreModule {}
